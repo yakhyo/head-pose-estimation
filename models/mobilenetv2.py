6f460b265c2d5c2f42a69c579ad4b23d046eb8f7
yakhyo <yakhyo9696@gmail.com>
2024-09-04 04:43:39 +0000

    feat: Add mobilenetv3

--- models/mobilenetv2.py
+++ models/mobilenetv2.py
@@ -2,61 +2,12 @@ import torch
 from torch import nn, Tensor
 from torchvision.models import MobileNet_V2_Weights
 
-from typing import Any, Callable, List, Optional
+from typing import Any,  List, Optional
 
 from utils.general import compute_rotation_matrix_from_ortho6d
-
+from .common import _make_divisible, Conv2dNormActivation, load_filtered_state_dict
 
 __all__ = ["mobilenet_v2"]
-
-
-def _make_divisible(v: float, divisor: int = 8) -> int:
-    """This function ensures that all layers have a channel number divisible by 8"""
-    new_v = max(divisor, int(v + divisor / 2) // divisor * divisor)
-    # Make sure that round down does not go down by more than 10%.
-    if new_v < 0.9 * v:
-        new_v += divisor
-    return new_v
-
-
-class Conv2dNormActivation(torch.nn.Sequential):
-    """Convolutional block, consists of nn.Conv2d, nn.BatchNorm2d, nn.ReLU"""
-
-    def __init__(
-            self,
-            in_channels: int,
-            out_channels: int,
-            kernel_size: int = 3,
-            stride: int = 1,
-            padding: Optional = None,
-            groups: int = 1,
-            activation_layer: Optional[Callable[..., torch.nn.Module]] = torch.nn.ReLU,
-            dilation: int = 1,
-            inplace: Optional[bool] = True,
-            bias: bool = False,
-    ) -> None:
-
-        if padding is None:
-            padding = (kernel_size - 1) // 2 * dilation
-
-        layers: List[nn.Module] = [
-            nn.Conv2d(
-                in_channels=in_channels,
-                out_channels=out_channels,
-                kernel_size=kernel_size,
-                stride=stride,
-                padding=padding,
-                dilation=dilation,
-                groups=groups,
-                bias=bias,
-            ),
-            nn.BatchNorm2d(num_features=out_channels, eps=0.001, momentum=0.01)
-        ]
-
-        if activation_layer is not None:
-            params = {} if inplace is None else {"inplace": inplace}
-            layers.append(activation_layer(**params))
-        super().__init__(*layers)
 
 
 class InvertedResidual(nn.Module):
@@ -175,10 +126,10 @@ class MobileNetV2(nn.Module):
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
 
         # building classifier
-        # self.classifier = nn.Sequential(
-        #     nn.Dropout(p=dropout),
-        #     nn.Linear(self.last_channel, num_classes),
-        # )
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=dropout),
+            # nn.Linear(self.last_channel, num_classes),
+        )
 
         self.linear_reg = nn.Linear(self.last_channel, num_classes)
 
@@ -200,25 +151,11 @@ class MobileNetV2(nn.Module):
         # Cannot use "squeeze" as batch-size can be 1
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
-
-        # Original FC layer from MobileNet V2
-        # x = self.classifier(x)
+        
+        x = self.classifier(x)
 
         x = self.linear_reg(x)
         return compute_rotation_matrix_from_ortho6d(x)
-
-
-def load_filtered_state_dict(model, state_dict):
-    """Update the model's state dictionary with filtered parameters.
-
-    Args:
-        model: The model instance to update (must have `state_dict` and `load_state_dict` methods).
-        state_dict: A dictionary of parameters to load into the model.
-    """
-    current_model_dict = model.state_dict()
-    filtered_state_dict = {key: value for key, value in state_dict.items() if key in current_model_dict}
-    current_model_dict.update(filtered_state_dict)
-    model.load_state_dict(current_model_dict)
 
 
 def mobilenet_v2(*, pretrained: bool = True, progress: bool = True, **kwargs: Any) -> MobileNetV2:
--- models/resnet.py
+++ models/resnet.py
@@ -5,6 +5,7 @@ from torchvision.models import ResNet18_Weights, ResNet34_Weights, ResNet50_Weig
 from typing import Any, Callable, List, Optional, Type, Tuple
 
 from utils.general import compute_rotation_matrix_from_ortho6d
+from .common import load_filtered_state_dict
 
 __all__ = ["resnet18", "resnet34", "resnet50"]
 
@@ -248,19 +249,6 @@ class ResNet(nn.Module):
         x = self.linear_reg(x)
 
         return compute_rotation_matrix_from_ortho6d(x)
-
-
-def load_filtered_state_dict(model, state_dict):
-    """Update the model's state dictionary with filtered parameters.
-
-    Args:
-        model: The model instance to update (must have `state_dict` and `load_state_dict` methods).
-        state_dict: A dictionary of parameters to load into the model.
-    """
-    current_model_dict = model.state_dict()
-    filtered_state_dict = {key: value for key, value in state_dict.items() if key in current_model_dict}
-    current_model_dict.update(filtered_state_dict)
-    model.load_state_dict(current_model_dict)
 
 
 def _resnet(block: Type[BasicBlock], layers: List[int], weights: Optional[ResNet34_Weights], progress: bool, **kwargs: Any) -> ResNet:
